828ad88acd489e6cdd76b96113314fc3f34b786b
Shane P Drafahl <drafahls@gmail.com>
2016-08-28 23:37:08 -0500

    Code for middle man program.

--- FairOrderingSystem/Controllers/HomeController.cs
+++ FairOrderingSystem/Controllers/HomeController.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Mvc.Ajax;
-
+using System.Diagnostics;
+//http://stackoverflow.com/questions/878632/best-way-to-call-external-program-in-c-sharp-and-parse-output
 namespace Fair.Controllers
 {
 	public class HomeController : Controller
@@ -28,6 +29,22 @@ namespace Fair.Controllers
 		}
 		public ActionResult LoginS(){
 			return View ();
+		}
+		public void creatUser(){
+
+			//string company = Request.Form ["company"];
+			string company = "Iowa State";
+			string cell = "6414";
+			string phone = "641521";
+			string password = "test";
+			string email = "iowa@isu.edu";
+			Process p = new Process ();
+			p.StartInfo.FileName = "/home/shanedrafahl/project/Iowa_State_Fair_OrderingSystem/CppMiddleMan/cppsql";
+			p.StartInfo.Arguments = "0 " + phone + " 0 " + company + " " + cell + " " + password + " " + email;
+			p.StartInfo.UseShellExecute = false;
+			p.StartInfo.RedirectStandardOutput = true;
+			p.Start();
+			p.WaitForExit ();
 		}
 	}
 }
